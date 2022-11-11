bedd63d54d8e884f12710abe4ce88150727a8ea5
Jostein <89195707+JosteinEng@users.noreply.github.com>
2022-11-11 14:10:36 +0100

    Update AccountController.cs

--- bacit-dotnet.MVC/Controllers/AccountController.cs
+++ bacit-dotnet.MVC/Controllers/AccountController.cs
@@ -124,7 +124,8 @@ namespace bacit_dotnet.MVC.Controllers
                     //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
                     //await _emailSender.SendEmailAsync(model.Email, "Confirm your account",
                     //    "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
 
                     
                     _logger.LogInformation(3, "User created a new account with password.");
