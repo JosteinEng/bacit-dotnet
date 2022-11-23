405e29b71af40351a4e1463a58d1b7de65f0fd14
JosteinEngesæt <josteinengesaet@gmail.com>
2022-11-23 14:20:54 +0100

    maxcap username password

--- bacit-dotnet.MVC/Models/Account/LoginViewModel.cs
+++ bacit-dotnet.MVC/Models/Account/LoginViewModel.cs
@@ -8,10 +8,12 @@ namespace bacit_dotnet.MVC.Models.Account;
 public class LoginViewModel
 {
     [Required(ErrorMessage = "Vennligst fyll inn ansattnummer")]
+    [StringLength(8, MinimumLength = 1)]
     public string Email { get; set; }
 
     [Required(ErrorMessage = "Vennligst skriv inn et passord")]
     [DataType(DataType.Password)]
+    [StringLength(128, ErrorMessage = "{0} må være minst {2} karakterer langt.", MinimumLength = 6)]
     public string Password { get; set; }
 
     [Display(Name = "Husk meg?")]
--- bacit-dotnet.MVC/Models/Account/RegisterViewModel.cs
+++ bacit-dotnet.MVC/Models/Account/RegisterViewModel.cs
@@ -13,26 +13,30 @@ public class RegisterViewModel
 {
     [Required(ErrorMessage = "Vennligst legg inn et ansattnummer.")]
     [Display(Name = "Ansattnummer")]
+    [StringLength(8, MinimumLength = 1)]
     public string Email { get; set; }
 
     [Required(ErrorMessage = "Vennligst skriv inn fornavnet til den ansatte.")]
     [Display(Name = "Fornavn")]
+    [StringLength(32, MinimumLength = 2)]
     public string FirstName { get; set; }
 
     [Required(ErrorMessage = "Vennligst skriv inn etternavnet til den ansatte.")]
     [Display(Name = "Etternavn")]
+    [StringLength(32, MinimumLength = 2)]
     public string LastName { get; set; }
     [Required]
     [Display(Name = "Rolle")]
     public string Role { get; set; }
 
     [Required(ErrorMessage = "Vennligst skriv inn et passord")]
-    [StringLength(100, ErrorMessage = "{0} må være minst {2} karakterer langt.", MinimumLength = 6)]
+    [StringLength(128, ErrorMessage = "{0} må være minst {2} karakterer langt.", MinimumLength = 6)]
     [DataType(DataType.Password)]
     [Display(Name = "Passord")]
     public string Password { get; set; }
 
     [Required(ErrorMessage = "Vennligst repeter det samme passordet")]
+    [StringLength(128)]
     [DataType(DataType.Password)]
     [Display(Name = "Confirm password")]
     [Compare("Password", ErrorMessage = "Det er et misforhold mellom passordene")]
